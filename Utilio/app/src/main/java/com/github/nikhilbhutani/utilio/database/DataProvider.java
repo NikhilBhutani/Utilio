4b121a1ca147c2a08530aab2339213afcdf2f8db
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-10-28 13:47:03 +0530

    checking

--- Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/database/DataProvider.java
+++ Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/database/DataProvider.java
@@ -4,6 +4,8 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -41,7 +43,7 @@ public class DataProvider extends ContentProvider{
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
         Cursor retCursor;
 
@@ -51,6 +53,19 @@ public class DataProvider extends ContentProvider{
         switch (mUriMatcher.match(uri)){
 
             case APPDATA:
+            {
+                retCursor = dataDbHelper.getReadableDatabase().query(
+                  DataContract.ApplicationData.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+
+                break;
+            }
 
         }
 
@@ -66,6 +81,16 @@ public class DataProvider extends ContentProvider{
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
+
+        final SQLiteDatabase db = dataDbHelper.getWritableDatabase();
+        final int match = mUriMatcher.match(uri);
+
+        Uri returnUri;
+
+        switch(match){
+            
+        }
+
         return null;
     }
 
