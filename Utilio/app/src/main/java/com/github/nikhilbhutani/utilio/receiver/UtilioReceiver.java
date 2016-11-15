45e55bab740bf6ff2ea7bbe012817b4becc8578a
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-11-16 00:36:42 +0530

    Changes in Fitness DashBoard

--- Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/receiver/UtilioReceiver.java
+++ Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/receiver/UtilioReceiver.java
@@ -20,26 +20,20 @@ public class UtilioReceiver extends BroadcastReceiver{
 
             if(intent.getAction().equals(Intent.ACTION_USER_PRESENT)){
 
-                /*
                 Intent localIntentUsrPrsnt = new Intent(context, UtilioService.class);
                 localIntentUsrPrsnt.putExtra("service", true);
                 context.startService(localIntentUsrPrsnt);
-                 */
             }
 
             if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF))
             {
-
-                /*
                 System.out.println("****** STOPING SERVICE BABY *************");
                 context.startService(new Intent(context, UtilioService.class));
-
-
 
                 Intent intentScreenOff = new Intent(context, UtilioService.class);
                 intentScreenOff.putExtra("screenoff",true);
                 context.startService(intentScreenOff);
-                  */
+
             }
 
         }
