d0c1187d493f0d3b278ee1a504b8f8489ef58270
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-11-22 01:18:36 +0530

    starting with widget

---
+++ Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/ui/activities/WidgetActivity.java
@@ -0,0 +1,12 @@
+package com.github.nikhilbhutani.utilio.ui.activities;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Nikhil Bhutani on 11/22/2016.
+ */
+
+public class WidgetActivity extends AppWidgetProvider {
+
+
+}
