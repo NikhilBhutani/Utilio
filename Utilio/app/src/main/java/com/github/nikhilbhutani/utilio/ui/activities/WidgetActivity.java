966617b81c17162f8cd5c7ed1b32a5a09d55fbe4
Nikhil Bhutani <niks.bhutani@gmail.com>
2016-11-23 02:37:37 +0530

    widget methods

--- Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/ui/activities/WidgetActivity.java
+++ Utilio/app/src/main/java/com/github/nikhilbhutani/utilio/ui/activities/WidgetActivity.java
@@ -1,6 +1,8 @@
 package com.github.nikhilbhutani.utilio.ui.activities;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 
 /**
  * Created by Nikhil Bhutani on 11/22/2016.
@@ -8,5 +10,19 @@ import android.appwidget.AppWidgetProvider;
 
 public class WidgetActivity extends AppWidgetProvider {
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
 }
